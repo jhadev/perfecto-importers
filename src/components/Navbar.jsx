ce588153a68341d506380dcaf0f17249438b41dd
JA <joshappeldev@gmail.com>
2018-12-18 16:36:24 -0500

    fixed margin for cart in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,20 +9,22 @@ class Navbar extends Component {
           <span className="company navbar-brand mb-0 h1 text-light">
             Perfecto Importers{" "}
           </span>
-          <p className="navbar-brand">
-            <span className="badge badge-pill badge-secondary">
-              Your Total: ${this.props.totalPrice}
-            </span>
-            <span className="badge badge-pill badge-secondary">
-              Total Cases: {this.props.totalCases}
-            </span>
-            <button
-              onClick={this.props.onReset}
-              className="btn reset badge-pill badge-secondary btn-sm"
-            >
-              Empty Cart
-            </button>
-          </p>
+          <div className="cart mt-3">
+            <p className="navbar-brand">
+              <span className="badge badge-pill badge-secondary">
+                Your Total: ${this.props.totalPrice}
+              </span>
+              <span className="badge badge-pill badge-secondary">
+                Total Cases: {this.props.totalCases}
+              </span>
+              <button
+                onClick={this.props.onReset}
+                className="btn reset badge-pill badge-secondary btn-sm"
+              >
+                Empty Cart
+              </button>
+            </p>
+          </div>
         </nav>
       </React.Fragment>
     );
