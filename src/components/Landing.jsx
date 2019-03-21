0c106e5c7f5aa337b479ac78f3998a81cdbbcb32
KN <knny.nguyen@gmail.com>
2019-03-20 21:20:23 -0400

    Flex

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 
+const background =
+  'https://images.pexels.com/photos/1410138/pexels-photo-1410138.jpeg?cs=srgb&dl=blueberries-bunch-close-up-1410138.jpg&fm=jpg';
+
+const row = {
+  display: 'flex'
+};
+
+const columnOne = {
+  display: 'flex',
+  width: '50%',
+  height: '92vh',
+  backgroundImage: `url(${background})`,
+  backgroundSize: 'cover',
+  justifyContent: 'center',
+  alignItems: 'center'
+};
+
+const columnTwo = {
+  display: '50%',
+  width: '50%',
+  height: '92vh'
+};
+
 class Landing extends Component {
   render() {
     return (
-      <div>
-        <h1>This Is a Landing Page!</h1>
+      <div style={row}>
+        <div style={columnOne}>
+          <h1 style={{ transform: 'rotate(-10deg)' }}>BUY OUR WINE</h1>
+        </div>
+        <div style={columnTwo}>
+          <h1>I AM ON THE RIGHT SIDE AND I WANT TO BE 3 COLUMNS TELLING YOU WHY, WHERE, AND HOW</h1>
+        </div>
       </div>
     );
   }
--- src/components/Titlebar.jsx
+++ src/components/Titlebar.jsx
@@ -1,12 +1,16 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Titlebar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-light sticky-nav bg-dark">
+      <nav style={{ height: '8vh' }} className="navbar navbar-light sticky-nav bg-dark">
         <span className="company navbar-brand mb-0 h1 text-light">
-          Perfecto Importers{" "}
+          <Link to="/">Perfecto Importers </Link>
         </span>
+        <li className="ml-auto nav-item company text-light">
+          <Link to="/winecart">Shop</Link>
+        </li>
       </nav>
     );
   }
